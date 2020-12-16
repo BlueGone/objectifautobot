21445e91b2a5397fdca2d954e5910cb7486d6e62
Thomas Plaçais <thomas.placais@protonmail.com>
2020-12-16 08:05:11 +0100

    Extract nb signatures from page

--- src/senatePetitionRetriever.ts
+++ src/senatePetitionRetriever.ts
@@ -1,10 +1,38 @@
 import got from "got";
+import cheerio from "cheerio";
 
 export type PetitionId = `i-${number}`;
+
+export interface PetitionData {
+  nbSignatures: number;
+  maxSignatures: number;
+} 
 
 export function retrievePetitionPage(petitionId: PetitionId): Promise<string> {
   const url = `https://petitions.senat.fr/initiatives/${petitionId}`;
   
   return got(url)
     .then(res => res.body);
+}
+
+export async function retrievePetitionData(petitionId: PetitionId): Promise<PetitionData> {
+  const regex = /(?<nbSignatures>\d+)\s*\/\s*(?<maxSignatures>\d+)\s*/
+
+  const petitionPage = await retrievePetitionPage(petitionId);
+  const $ = cheerio.load(petitionPage);
+  const signaturesContainer = $('div.progress__bar__title');
+
+  const match = regex.exec(signaturesContainer.text());
+
+  if (match && match.groups) {
+    const nbSignatures = + match.groups.nbSignatures;
+    const maxSignatures = + match.groups.maxSignatures;
+
+    return {
+      nbSignatures,
+      maxSignatures,
+    };
+  }
+
+  throw new Error('retrievePetitionData');
 }
