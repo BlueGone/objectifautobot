b8da821019b7bf6de1baa3d1b7b0346dabdab2de
Thomas Plaçais <thomas.placais@protonmail.com>
2020-12-17 22:44:50 +0100

    Show remaining days in status

--- src/status.ts
+++ src/status.ts
@@ -1,14 +1,19 @@
+import { differenceInCalendarDays } from "date-fns";
 import { getPetitionLink, PetitionData, PetitionId } from "./senatePetitionRetriever";
+
+const petitionDeadline = new Date(2021, 3, 10);
 
 export const makeStatusFromPetitionData = (id: PetitionId) => (data: PetitionData) => {
   const { nbSignatures, maxSignatures } = data;
 
   const progression = Math.floor(nbSignatures * 100 / maxSignatures);
 
+  const nbRemainingDays = differenceInCalendarDays(petitionDeadline, Date.now())
+
   return (
 `
 Déjà ${nbSignatures} signatures sur les ${maxSignatures} requises, soit ${progression}%.
-Si vous n'avez pas encore signé, il est encore temps.
+Il reste ${nbRemainingDays} jours, si vous n'avez pas encore signé, il est encore temps.
 
 ${getPetitionLink(id)}
 `
